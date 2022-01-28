f4dc197f9efafc8af04b34fe5415be3217f1e560
Callum Reid <callum.reid92@hotmail.com>
2022-01-28 12:28:36 +1100

    Completed module

--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,26 @@
 function Util() {
 
+    function calculateTotalDistributedPassengers(distributeAllSeatsToAllPassengers){
+        let totalDistributedPassengers = 0;
+        let value;
+
+        for (value in distributedPassengers) {
+            totalDistributedPassengers += distributedPassengers[value];
+        }
+        return totalDistributedPassengers;
+    }
+
+    function calculateTotalNumberOfPassengers(passengersArray) {
+        let totalNumberOfPassengers = 0;
+        let passengers;
+
+        for (passenger of passengersArray) {
+            totalNumberOfPassengers += passengers;
+        }
+        return totalNumberOfPassengers;
+    }
+
+    return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
 }
 
+module.exports = Util();
